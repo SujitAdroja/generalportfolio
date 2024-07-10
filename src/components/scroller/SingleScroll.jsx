4e789fe90a61b51b3092acabdd6ab2cf1458113a
Sujit Adroja <sujitadroja07@gmail.com>
2024-07-10 17:47:38 +0530

    changes

---
+++ src/components/scroller/SingleScroll.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+
+// function SingleScroll({ dataDirection, scrollSpeed, textColor }) {
+//   console.log("dataDirection", scrollSpeed);
+//   return (
+//     <div
+//       className="scroller"
+//       data-speed={scrollSpeed ? "fast" : "slow"}
+//       data-direction={dataDirection}
+//     >
+//       <ul className={`tag-list scroller__inner text-[${textColor}]`}>
+//         <li>HTML</li>
+//         <li>CSS</li>
+//         <li>JavaScript</li>
+//         <li>SSG</li>
+//         <li>WebDev</li>
+//         <li>Animation</li>
+//         <li>UI/UX</li>
+//         <li>Illustrations</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default SingleScroll;
